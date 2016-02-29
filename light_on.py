993e957ad07ac0f74f0a07635d498ae6a7db44c2
rener2 <rene1047@gmail.com>
2016-02-29 16:28:05 +0200

    Delete light_on.py

--- light_on.py
+++
@@ -1,9 +0,0 @@
-#!/home/pi
-import time
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
-GPIO.setwarnings(False)
-GPIO.setup(17, GPIO.OUT) 
-GPIO.output(17, GPIO.HIGH)  ## Lights on
-        
