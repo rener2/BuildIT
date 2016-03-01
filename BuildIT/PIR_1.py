950b139b69ffdf6771d74421dcd1dac60e2d95c2
Hendrig Sellik <sellikhendrig@gmail.com>
2016-03-01 21:19:21 +0200

    Added files via upload
    new start PIR_1.py

--- BuildIT/PIR_1.py
+++ BuildIT/PIR_1.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python
 #!/home/pi
 import time
 import sys
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.IN) 
-print(GPIO.input(18))
+GPIO.setup(4, GPIO.IN) 
+print(GPIO.input(4))
