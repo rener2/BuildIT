3f963e920595b10fec058067afed4aed3102247f
Hendrig Sellik <sellikhendrig@gmail.com>
2016-03-01 21:18:53 +0200

    Added files via upload
    new start light_off.py

--- BuildIT/light_off.py
+++ BuildIT/light_off.py
@@ -1,9 +1,9 @@
-#!/home/pi
+#!/usr/bin/python
+#!/root
 import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 GPIO.setwarnings(False)
-GPIO.setup(17, GPIO.OUT) 
-GPIO.output(17, GPIO.LOW)  
-        
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, GPIO.LOW)  # Lights off
