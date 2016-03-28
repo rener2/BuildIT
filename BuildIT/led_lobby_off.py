1ece8d777e227adde90fbe937a1a49e3e7469be2
unknown <sellikhendrig@gmail.com>
2016-03-28 13:10:24 +0300

    imported devices and wrote according to our standards

--- BuildIT/led_lobby_off.py
+++ BuildIT/led_lobby_off.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 #!/root
+import devices
 import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
-led_pin = 25
+led_pin = devices.LED_LOBBY
 GPIO.setwarnings(False)
 GPIO.setup(led_pin, GPIO.OUT)
 GPIO.output(led_pin, GPIO.LOW)  # Lights off
