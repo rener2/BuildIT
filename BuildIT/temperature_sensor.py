af148516008052ccd52fc1639e6f0d0457ec9c52
unknown <sellikhendrig@gmail.com>
2016-03-29 16:55:02 +0300

    removed C after temp

--- BuildIT/temperature_sensor.py
+++ BuildIT/temperature_sensor.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 #!/root
+#!/usr/bin/python
+#!/root
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+GPIO.setwarnings(False)
+GPIO.setup(4, GPIO.IN)
 import Adafruit_DHT as dht
-import devices
-h, t = dht.read_retry(dht.DHT22, devices.TEMP_SENSOR)
-print('{0:0.1f}*C'.format(t))
+h,t = dht.read_retry(dht.DHT22, 4)
+print('{0:0.1f}'.format(t))
