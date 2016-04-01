001d2d41fb990ed72622564479ec25af013dc398
Rene Räkk <rene1047@gmail.com>
2016-04-01 17:25:25 +0300

    needs testing

---
+++ BuildIT/peltier_on.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#!/root
+import RPi.GPIO as GPIO
+import devices
+GPIO.setmode(GPIO.BCM)
+peltier_pin = devices.PELTIER
+GPIO.setwarnings(False)
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.output(led_pin, GPIO.HIGH)  # heating on
