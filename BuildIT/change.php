34517ef81cedcb474b76fff8912d9a6d36698ff9
Rene Räkk <rene1047@gmail.com>
2016-04-07 20:06:25 +0300

    overwrite configuration.yaml
    need to add database queries and remove 0 from the end.

---
+++ BuildIT/change.php
@@ -0,0 +1,28 @@
+<?php
+	$alias = '- alias: ';
+	// + data from database
+	$trigger = ''; 
+	// + data from database
+	$automation = '';
+	// + data from database
+	
+	$new_alias = $alias + '\n' + $trigger + '\n' + $automation + '\n'; 
+	 
+	$old_data = '';
+    // load the data and delete the line from the array 
+    $lines = fopen('C:\xampp\htdocs\Configuration.yaml', 'r'); 
+	$old_data = '';
+	
+	for ($i=0; $i < 133; $i++) {
+		$line = fgets($lines);
+		#if ($line != 0) {
+		$old_data .= $line;
+		#}
+		}
+	
+	$old_data .= $new_alias;
+		
+    $fp = fopen('C:\xampp\htdocs\Configuration.yaml', 'w'); 
+    fwrite($fp, $old_data); 
+    fclose($fp); 	
+?>
