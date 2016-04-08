627f2d009b49f0bba3af8c9f22c3759da0e269cd
unknown <sellikhendrig@gmail.com>
2016-04-08 16:44:33 +0300

    added none check

--- BuildIT/humidity_sensor.py
+++ BuildIT/humidity_sensor.py
@@ -4,5 +4,7 @@ import time
 import Adafruit_DHT as dht
 import devices
 humidity, temperature = dht.read_retry(dht.DHT22, devices.HUMIDITY_SENSOR)
-if (humidity is not None & temperature is not None):
+humidity = None
+temperature = None
+if (humidity is not None and temperature is not None):
     print('{1:0.1f}'.format(temperature - 1, humidity))
