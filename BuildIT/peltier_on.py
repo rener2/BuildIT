015e055b78d2d2f3514c2ed405bbee393a44483f
unknown <sellikhendrig@gmail.com>
2016-04-09 19:32:24 +0300

    fixed peltier bug
    replaced led_pin with peltier_pin

--- BuildIT/peltier_on.py
+++ BuildIT/peltier_on.py
@@ -5,5 +5,5 @@ import devices
 GPIO.setmode(GPIO.BCM)
 peltier_pin = devices.PELTIER
 GPIO.setwarnings(False)
-GPIO.setup(led_pin, GPIO.OUT)
-GPIO.output(led_pin, GPIO.HIGH)  # heating on
+GPIO.setup(peltier_pin, GPIO.OUT)
+GPIO.output(peltier_pin, GPIO.HIGH)  # heating on
