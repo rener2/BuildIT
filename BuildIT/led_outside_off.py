cc41a2f2d563c4fedd5853fc302564767045633e
unknown <sellikhendrig@gmail.com>
2016-04-09 12:29:46 +0300

    changed led lobby to led outside

---
+++ BuildIT/led_outside_off.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#!/root
+import devices
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+#GPIO.cleanup()
+led_pin = devices.LED_OUTSIDE
+GPIO.setwarnings(False)
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.output(led_pin, GPIO.LOW)  # Lights off
--- BuildIT/light_sensor.py
+++ BuildIT/light_sensor.py
@@ -3,6 +3,6 @@
 import smbus
 import time
 bus = smbus.SMBus(0)
-address = 0x70
+address = 8x40
 light = bus.read_byte_data(address, 1)
 print light
